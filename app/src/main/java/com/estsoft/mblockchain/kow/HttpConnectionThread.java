758dc5f88135a99c9fee55b43fcc3eb3dd040ab8
YeonJi <yjhwo3@gmail.com>
2016-11-22 17:35:06 +0900

    Include httpconnThread

---
+++ app/src/main/java/com/estsoft/mblockchain/kow/HttpConnectionThread.java
@@ -0,0 +1,96 @@
+package com.estsoft.mblockchain.kow;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by yeonji on 2016-11-21.
+ * HttpConnectionThread
+ * post방식으로 넘길 경우
+ */
+
+public class HttpConnectionThread extends AsyncTask<String, Void, String> {
+    private static String LOG_TAG = "HttpConnectionThread";
+    static Context mContext;
+
+    public HttpConnectionThread(Context c) {
+        mContext = c;
+    }
+
+    @Override
+    protected String doInBackground(String... path){
+        // URL 연결이 구현될 부분
+        URL url;
+        String response = "";
+        String CONNURL = path[0];
+        String VALUE = path[1];
+        HttpURLConnection conn = null;
+
+        try {
+            url = new URL(CONNURL);
+            Log.e(LOG_TAG, CONNURL);
+            Log.e(LOG_TAG, VALUE);
+
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(3000); // 타임아웃
+            conn.setRequestMethod("POST");
+//            conn.setRequestProperty("Cache-Control", "no-cache");
+//            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            OutputStream os = conn.getOutputStream(); // 서버로 보내기 위한 출력 스트림
+            os.write(VALUE.getBytes());
+            os.flush();
+            os.close();         /**  <<<< 위치 */
+
+            Log.e("http response code", conn.getResponseCode()+"");
+            if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) { // 연결에 성공한 경우
+                Log.e(LOG_TAG, "연결 성공");
+                String line;
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream())); // 서버의 응답을 읽기 위한 입력 스트림
+
+                while ((line = br.readLine()) != null) {// 서버의 응답을 읽어옴
+                    response += line;
+                }
+
+                br.close();
+                conn.disconnect();
+                Log.e("RESPONSE", "The response is: " + response);
+            }else{
+                response = "failed";
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            conn.disconnect();
+        }
+
+        return response;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        // UI 업데이트가 구현될 부분
+
+        if( result.equals("failed")){
+            Log.e("연결","실패");
+        }else if( !result.equals("{}")) {
+            Log.e("!{}","진입");
+        }
+
+
+    }
+
+}   // End_HttpConnectionThread
--- app/src/main/java/com/estsoft/mblockchain/kow/InputTextActivity.java
+++ app/src/main/java/com/estsoft/mblockchain/kow/InputTextActivity.java
@@ -1,13 +1,113 @@
 package com.estsoft.mblockchain.kow;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.util.Date;
+
+/**
+ * Created by yeonji on 2016-11-22.
+ * This Activity is received user's text input.
+ */
 
 public class InputTextActivity extends AppCompatActivity {
+
+    private static String schInsertURL = "http://192.168.22.72/insertUserDocu.php";
+    private int generatedNum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_text);
+
+        final EditText edt_input = (EditText)findViewById(R.id.edt_input);
+        Button btn_enroll = (Button)findViewById(R.id.btn_enroll);
+        TextView txt_date = (TextView)findViewById(R.id.txt_date);
+
+        Date now = new Date();
+        DateFormat frm = DateFormat.getDateInstance(DateFormat.LONG);
+        txt_date.setText(frm.format(now));
+
+        btn_enroll.setOnClickListener( v -> {
+            String input = edt_input.getText().toString();
+            Log.e("input", input);
+
+            generateTextFile(input);
+
+            /*************************************************************************************/
+            String userID = "yjhwo";        // <<< user db와 연동하기!
+            /*************************************************************************************/
+
+            // -- 보낼 데이터
+            String data = "id="+userID+"&text="+input;
+            new HttpConnectionThread(getApplicationContext()).execute(schInsertURL,  data);
+
+        });
+
+
     }
+
+    private void generateTextFile(String input) {
+
+        setFileNum();          // Generate File Number
+
+        // Generate File Name
+        // rule : user id + generated num
+        String userID = "yjhwo";
+
+        String fileName = userID + generatedNum+".txt";
+        Log.e("To generate fileName", fileName);
+
+        try{
+            // 파일 생성
+            FileOutputStream fos = openFileOutput(fileName, Context.MODE_WORLD_READABLE);
+            fos.write(input.getBytes());            // <<<<<여기 수정
+            fos.close();
+
+            Toast.makeText(getApplicationContext(), "Save Success", Toast.LENGTH_SHORT).show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void setFileNum() {
+
+        // 파일들 읽어와서 없는 번호부터 생성해줌
+        String dirPath = getFilesDir().getPath();
+        File file = new File(dirPath);
+        Log.e("dirPath", dirPath);
+
+        // 파일이 1개 이상이면 파일 이름 출력
+        Log.e("listFiles.length",file.listFiles().length+"");
+
+        File[] list = file.listFiles();         //폴더가 가진 파일객체를 리스트로 받는다.
+
+        for (File f : list) {
+            if( f.isFile() ) {                  // only file
+                String str = f.getName();
+
+                if( str.contains(".txt") ){     //확장자명 .txt인 파일만 확인
+                    generatedNum++;
+                }
+            }
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        generatedNum = 0;
+    }
+
 }
