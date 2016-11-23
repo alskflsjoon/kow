2c1a6606c3e66999fc9b30528edf67ae8cf1254b
choiyoungjin <tyranosaurus@nate.com>
2016-11-23 22:22:30 +0900

    Addressbook : 첫 번째 커밋
    - Manirfest : 주소록 엑세스, 전화걸기 및 관리 권한 등록
    - MainActivity : 권한 요청 코드 추가
    - WaitingFragment : 권한 요청 코드 추가
    - FCMTestActivity : 돌아가기 버튼 눌렸을때 동작 수정

--- app/src/main/java/com/estsoft/mblockchain/kow/MainActivity.java
+++ app/src/main/java/com/estsoft/mblockchain/kow/MainActivity.java
@@ -1,23 +1,34 @@
 package com.estsoft.mblockchain.kow;
 
+import android.*;
+import android.Manifest;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -29,9 +40,14 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
 import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-/* 병합 테스트 */
+
     private FloatingActionButton fab;
+
+    /* Added code for Sync Addressbook : 2016-11-23 by 최영진*/
+    final int MainActivity_RequestCode = 100;
 
 
     @Override
@@ -50,6 +66,8 @@ public class MainActivity extends AppCompatActivity {
         // receive Token by each user
         FirebaseInstanceId.getInstance().getToken(); // 이 함수가 불리는 시점에서 DB에 토큰이 저장됨
 
+        /* Added code for Sync Addressbook : 2016-11-23 by 최영진*/
+        syncAddressbook();
 
     }
 
@@ -163,6 +181,100 @@ public class MainActivity extends AppCompatActivity {
 
     private void updatePage(int selectedPage) {
         Toast.makeText(this,"updatePage"+selectedPage,Toast.LENGTH_LONG).show();
+    }
+
+    /* Added code for Sync Addressbook : 2016-11-23 by 최영진*/
+    private void syncAddressbook() {
+        // 요청하고자 하는 권한들
+        String[] permissions = new String[] {
+                android.Manifest.permission.READ_CONTACTS,
+                android.Manifest.permission.READ_PHONE_STATE
+        };
+
+        // 안드로이드 버전이 마시멜로 이상인지 체크 : 마시멜로 미만에서는 런타임 요청 없이 바로 쿼리를 실행한다.
+        if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
+
+            ArrayList<String> reqPermissionList = new ArrayList<>();
+
+            // 안드로이드 버전이 마시멜로 이상인 경우
+            for ( String permission : permissions ) {
+                Log.e("-------onCreate", permission);
+                // 권한 허용여부 체크 : 허용 0 반환, 거부 -1 반환
+                int result = PermissionChecker.checkSelfPermission(MainActivity.this, permission);
+
+                if ( result == PermissionChecker.PERMISSION_GRANTED ) {
+                    // 권한이 허용된 경우
+                    Log.e("-------onCreate", permission + "획득");
+
+                } else {
+                    // 권한이 거부된 경우
+                    reqPermissionList.add(permission);
+                }
+            }
+            getPermissions(reqPermissionList);
+        } else {
+            // 안드로이드 버전이 마시멜로 미만인 경우
+            Log.e("-------onCreate", "마쉬멜로 미만 버전(권한 요청 X)");
+        }
+    }
+
+    /* Added code for Sync Addressbook : 2016-11-23 by 최영진*/
+    // 권한 획득 성공 또는 실패 결과에 따른 처리 해주는 메소드
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case MainActivity_RequestCode: {
+                // If request is cancelled, the result arrays are empty.
+                for ( int i = 0; i < permissions.length; i++ ) {
+                    if ( grantResults.length > 0 && grantResults[i] == PackageManager.PERMISSION_GRANTED ) {
+                        // permission was granted, yay! Do the
+                        // contacts-related task you need to do.
+                        Log.e("onRequestPermissionsRes", permissions[i] + "획득");
+                        Log.e("onRequestPermissionsRes", grantResults[i]+"");
+                    } else {
+                        // permission denied, boo! Disable the
+                        // functionality that depends on this permission.
+                    }
+                }
+
+                if ( ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_DENIED ||
+                        ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.READ_CONTACTS) ||
+                        ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_PHONE_STATE)) {
+
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                    alertDialogBuilder.setTitle("권한 요청")
+                            .setMessage("주소록 동기화를 위해서 권한에 대한 허용이 필요합니다. " +
+                                    "\"다시 묻지 않기\"를 체크하시면 이후 \"설정->애플리케이션 관리\"에서 " +
+                                    "권한 설정을 할 수 있습니다.")
+                            .setCancelable(false) // back 키 터치했을때 다이얼로그 cancel 여부 설정, 디폴트는 true
+                            .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialogBuilder.show();
+                }
+                return;
+            }
+        }
+    }
+    /* Added code for Sync Addressbook : 2016-11-23 by 최영진*/
+    private void getPermissions(ArrayList<String> reqPermissionList) {
+        Log.e("-------getPermission함수", reqPermissionList+"");
+
+        String[] permissions = new String[reqPermissionList.size()];
+        for ( int i = 0; i < reqPermissionList.size(); i++ ) {
+            permissions[i] = reqPermissionList.get(i);
+        }
+
+        if ( reqPermissionList.size() > 0 ) {
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, MainActivity_RequestCode);
+        } else {
+            // 예외처리 : 요청할 권한이 없을 경우
+        }
     }
 
 
--- app/src/main/java/com/estsoft/mblockchain/kow/fragments/WaitingFragment.java
+++ app/src/main/java/com/estsoft/mblockchain/kow/fragments/WaitingFragment.java
@@ -1,10 +1,21 @@
 package com.estsoft.mblockchain.kow.fragments;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -173,6 +184,24 @@ public class WaitingFragment extends Fragment {
             }
         });
 
+        // [주소록 동기화 코드]--------------------------------------------------------------------
+        // 주소록 가져오는 버튼
+        Button bringAddressBook = (Button)rootView.findViewById(R.id.bringAddressBook);
+        bringAddressBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GetUserContactsList();
+            }
+        });
+
+        // 자기번호 가져오기
+        Button bringMyPhoneNumber = (Button)rootView.findViewById(R.id.bringMyPhoneNumber);
+        bringMyPhoneNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GetUserPhoneNumber();
+            }
+        });
 
         return rootView;
     }
@@ -240,5 +269,146 @@ public class WaitingFragment extends Fragment {
             }
         }
     }
-}
 
+    /* Added code for Sync Addressbook : 2016-11-23 by 최영진*/
+    public void GetUserContactsList() {
+        // 필요한 권한 : Manifest.permission.READ_CONTACTS;
+        // 권한 승인 안받은 경우 예외처리
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_DENIED ||
+                ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_CONTACTS)) {
+
+            Log.e("GetUserContactsList", ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_CONTACTS)+"");
+            // 권한 없음 & 권한 재요청 안내
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+            alertDialogBuilder.setTitle("[주소록 엑세스] 권한 요청")
+                    .setMessage("주소록 동기화를 위해 [주소록 엑세스] 권한이 필요합니다." +
+                            "\"설정->애플리케이션 관리->애플리케이션 관리자 -> 해당 앱 -> 권한\"에서 설정을 바꿀 수 있습니다.")
+                    .setCancelable(false)
+                    .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialogBuilder.show();
+
+            return;
+        }
+
+        // 연락처의 [ID, 이름] 을 저장하는 String 배열 변수
+        String[] arrProjection = {
+                ContactsContract.Contacts._ID, // ID 열에 해당 하는 정보. 저장된 각 사용자는 고유의 ID를 가진다.
+                ContactsContract.Contacts.DISPLAY_NAME // 연락처에 저장된 이름 정보.
+        };
+
+        // 연락처의 [전화번호] 를 저장하는 String 배열 변수
+        String[] arrPhoneProjection = {
+                ContactsContract.CommonDataKinds.Phone.NUMBER // 연락처에 저장된 전화번호 정보
+        };
+
+        // get user list
+        // 주소록에 기록된 연락처 정보 중 ID 와 저장된 이름을 가져오는 Cursor
+        // 이후 ID, 저장된 이름에 이어서 email 같은 부가적인 정보를 뒤에 붙인다.
+        Cursor clsCursor = getActivity().getContentResolver().query (
+                ContactsContract.Contacts.CONTENT_URI,
+                arrProjection, // 연락처의 [ID, 이름] 컬럼의 정보를 가져온다.
+                ContactsContract.Contacts.HAS_PHONE_NUMBER + "=1" , // HAS_PHONE_NUMBER : 하나 이상의 전화번호가 있으면 1, 그 외에는 0
+                null,
+                null
+        );
+        // Cursor.moveToNext() : Cursor를 다음 행(Row)으로 이동 시킨다. 다음행이 있으면 true, 없으면 false
+        // 더이상 출력할 연락처 정보가 없으면 false를 반환하면 while문을 빠져나간다.
+        while( clsCursor.moveToNext() )
+        {
+            // DB에 정의된 필드의 타입에 의해 int로 설정한 필드는 getInt, String으로 설정한 필드는 getString 으로 가져와야 한다.
+            // getInt(int index), getString(int index) ... : 쿼리에 따라 요청된 각 컬럼 순서대로 0, 1, 2.. 이렇게 index가 부여되고 index에 해당하는 컬럼의 값이 int나 String 등의 타입으로 가져와진다.
+            String strContactId = clsCursor.getString( 0 );
+
+            Log.d("Unity", "연락처 사용자 ID : " + clsCursor.getString( 0 ));
+            Log.d("Unity", "연락처 사용자 이름 : " + clsCursor.getString( 1 ));
+
+            // phone number에 접근하는 Cursor
+            Cursor clsPhoneCursor = getActivity().getContentResolver().query (
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                    arrPhoneProjection, // 연락처의 [전화번호] 컬럼의 정보를 가져온다.
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + strContactId, // where 절 : 연락처의 ID와 일치하는 전화번호를 가져온다.
+                    null,
+                    null
+            );
+
+            // 연락처에서 하나의 ID에 핸드폰, 집전화 등 전화번호가 하나 이상 있는 경우 while문으로 모두 출력한다.
+            // 더이상 출력할 연락처 번호가 없으면 false를 반환하면 while문을 빠져나간다.
+            while( clsPhoneCursor.moveToNext() )
+            {
+                Log.d("Unity", "연락처 사용자 번호 : " + clsPhoneCursor.getString( 0 ));
+            }
+            // 전화번호 정보에 접근하는 Cursor 닫는다.
+            clsPhoneCursor.close();
+        }
+        // ID, 이름 정보에 접근하는 Cursor 닫는다.
+        clsCursor.close( );
+    }
+
+    /* Added code for Sync Addressbook : 2016-11-23 by 최영진*/
+    public void GetUserPhoneNumber() {
+        // 내 번호를 저장할 변수
+        String phoneNum = "아무값도 안들어옴";
+
+        // 필요한 권한 : Manifest.permission.READ_PHONE_STATE;
+        // 권한 승인 안받은 경우 예외처리
+        if ( ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_DENIED ||
+                ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_PHONE_STATE) ) {
+            Log.e("GetUserPhoneNumber", ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_PHONE_STATE)+"");
+            // 권한 없음 & 권한 재요청 안내
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+            alertDialogBuilder.setTitle("[전화 걸기 및 관리] 권한 요청")
+                    .setMessage("주소록 동기화를 위해 [전화 걸기 및 관리] 권한이 필요합니다." +
+                            "\"설정->애플리케이션 관리->애플리케이션 관리자 -> 해당 앱 -> 권한\"에서 설정을 바꿀 수 있습니다.")
+                    .setCancelable(false)
+                    .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialogBuilder.show();
+
+            return;
+        }
+        // 내 핸드폰 번호를 가져와서 phoneNum에 저장한다.
+        TelephonyManager telManager = (TelephonyManager) getActivity().getSystemService(getActivity().TELEPHONY_SERVICE);
+        phoneNum = telManager.getLine1Number();
+
+        // 내 핸드폰 번호를 출력한다.
+        TextView myPhoneNumber = (TextView)getActivity().findViewById(R.id.myPhoneNumber);
+        myPhoneNumber.setText(phoneNum);
+
+        // 내 핸드폰 번호를 DB에 넣는다.
+        final String phoneNumber = phoneNum;
+        // 네트워크 exception이 발생해 Thread로 해결
+        new Thread() {
+            public void run() {
+                // OkHttpClient 를 사용하기 위해서는 Manifest에 INTERNET 권한을 추가하고,
+                // build.gradle 에 okhttp 컴파일 코드를 추가해야 한다.
+                // 참고 : OkHttpClient을 이용한 통신은 3G환경에서는 java.net.SocketTimeoutException 에러가 발생
+                //        Wifi 환경에서는 잘됨.
+                OkHttpClient client = new OkHttpClient();
+                RequestBody body = new FormBody.Builder()
+                        .add("phoneNumber", phoneNumber)
+                        .build();
+
+                //request
+                Request request = new Request.Builder()
+                        // input MAC address of Ethernet
+                        .url("http://192.168.22.73/addressbookphp/insertPhoneNumber.php")
+                        .post(body)
+                        .build();
+                try {
+                    client.newCall(request).execute();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+}
