22ddb10a2894cbac89fba1d8b9ce76f526d196f6
joeylee <leeyw910205@gmail.com>
2016-11-23 17:51:29 +0900

    fcm login merge success

--- app/src/main/java/com/estsoft/mblockchain/kow/MainActivity.java
+++ app/src/main/java/com/estsoft/mblockchain/kow/MainActivity.java
@@ -44,12 +44,11 @@ public class MainActivity extends AppCompatActivity {
         setupFab();
 
         /* Added code for FCM Push : 2016-11-23 by 최영진*/
-        /** 에러발생 ★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆
         // receive FCM Push by topic
         FirebaseMessaging.getInstance().subscribeToTopic("news");
         // receive Token by each user
         FirebaseInstanceId.getInstance().getToken(); // 이 함수가 불리는 시점에서 DB에 토큰이 저장됨
-        */
+
 
     }
 
@@ -123,10 +122,6 @@ public class MainActivity extends AppCompatActivity {
             Intent inputActivity = new Intent(getApplicationContext(), InputTextActivity.class);
             startActivity(inputActivity);
         } );
-
-        rlIcon1.setOnClickListener( v ->
-            Toast.makeText(this,"text",Toast.LENGTH_LONG).show()
-        );
         rlIcon2.setOnClickListener( v ->
             Toast.makeText(this,"picture",Toast.LENGTH_LONG).show()
         );
